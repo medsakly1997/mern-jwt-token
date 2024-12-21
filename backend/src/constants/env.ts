8adaad416a8bfbcca659a2d0bc87bb32ba2996a1
sakly mohamed khaled <saklymohamedkhaled@gmail.com>
2024-12-21 15:47:18 +0100

    feat(sakly): Configure Cookie Parser and CORS Middleware

--- backend/src/constants/env.ts
+++ backend/src/constants/env.ts
@@ -9,3 +9,4 @@ const getEnv = (key: string, defaultValue?: string): string => {
 export const MONGODB_URI = getEnv("MONGODB_URI");
 export const PORT = getEnv("PORT", "4000");
 export const NODE_ENV = getEnv("NODE_ENV", "development");
+export const APP_ORIGIN = getEnv("APP_ORIGIN", "http://localhost:4000");
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,23 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectToDatabase from "./config/db";
-import { NODE_ENV, PORT } from "./constants/env";
+import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
 dotenv.config();
 
 // Create a new express application instance
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: APP_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 // Define the root path with a greeting message
 app.get("/", (req: Request, res: Response) => {
